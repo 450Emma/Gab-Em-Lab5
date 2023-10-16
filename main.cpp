c43682e88c6bc0ba87885794090aff04d263158f
Gabriel-Munoz <147623894+Gabriel-Munoz@users.noreply.github.com>
2023-10-16 16:14:42 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,79 +1,86 @@
-// lab partner: Gabriel Munoz
-
 #include <iostream>
 #include <vector>
 #include "StringData.h"
-using namespace std;
 
-// function prototypes
-int linear_search(vector<string> container, string element);
-int binary_search(vector<string> container, string element);
-int run_searches(string input);
+using namespace std;
+// search through the list individually comparing each element
+int linear_search(vector<string> container, string element){
+    for(int i = 0; i < container.size() ; i++){
+        if (container[i] == element){
+            return i;
+        }
+    }
+    return -1;
+}
+
+// divides list in half and checks for the placement of the element
+// (either higher or lower) and repeats process
+int binary_search(vector<string> container, string element){
+    int low = 0;
+    int high = container.size() - 1;
+    while (low <= high){
+        int mid = (low + high) / 2;
+        if (container[mid] > element){
+            high = mid - 1;
+        }
+        else if(container[mid] < element){
+            low = mid + 1;
+        }
+        else{
+            return mid ;
+        }
+    }
+    return -1;
+}
+
+
 
 int main() {
+    // intialized list that is searched through plus the specific phrases
+    vector<string> string_list = getStringData();
+    string s1 = "not_here";
+    string s2 = "aaaaa";
+    string s3 = "mzzzz";
 
-    // runs binary and linear search for each string and displays resulting time and index
-    run_searches("not_here");
-    run_searches("mzzzz");
-    run_searches("aaaaa");
+    // creates and sets variable for start and end
+    chrono::time_point<chrono::system_clock> start, end;
 
-    return 0;
-}
+    for (int i = 0; i < 6; i++) {
+        string fiveString;
+        // checks the iteration and accordingly changes it after every two
+        if (i == 0) {
+            fiveString = s1;
+            cout << fiveString << " search:" << endl;
+        }
+        if (i == 2) {
+            fiveString = s2;
+            cout << fiveString << " search:" << endl;
+        }
+        if (i == 4) {
+            fiveString = s3;
+            cout << fiveString << " search:" << endl;
+        }
+        // checks whether the given iteration is odd or even so that for every term
+        // binary search as well as linear search is called,
+        // checks current time performs operation and subtracts time it took to complete the operation
 
-// runs binary and linear search for imputed string and displays resulting time and index
-int run_searches(string input){
-    vector<string> dataset = getStringData();
-    cout << "Searches for "<< input << endl;
+        if (i % 2 == 0) {
+            start = chrono::system_clock::now();
+            int ind = linear_search(string_list, fiveString);
+            chrono::duration<double> operation_time = chrono::system_clock::now() - start;
+            cout << "Linear Index: " << ind << endl;
+            cout << "Linear Time: " << operation_time.count() << endl;
+        }
 
-    // creates and sets variable type for start_time and end_time
-    chrono::time_point<std::chrono::system_clock> start_time, end_time;
+        if (i % 2 == 1) {
+            start = chrono::system_clock::now();
+            int ind = binary_search(string_list, fiveString);
+            chrono::duration<double> operation_time = chrono::system_clock::now() - start;
+            cout << "Binary Index: " << ind << endl;
+            cout << "Binary Time: " << operation_time.count() << endl;
+        }
 
-    // runs, times, and displays info for linear search
-    start_time = chrono::system_clock::now();
-    int index = linear_search(dataset, input);
-    chrono::duration<double> elapsed_time = chrono::system_clock::now() - start_time;
-    cout << "Linear time: " << elapsed_time.count() << endl;
-    cout << "Linear index: " << index << endl;
 
-    // runs, times, and displays info for binary search
-    start_time = chrono::system_clock::now();
-    index = binary_search(dataset, input);
-    elapsed_time = chrono::system_clock::now() - start_time;
-    cout << "Binary time: " << elapsed_time.count() << endl;
-    cout << "Binary index: " << index << endl;
-
-    cout << endl;
-    return 0;
-
-}
-
-// compares element to each value in container and returns index when found
-int linear_search(vector<string> container, string element) {
-    for (int i = 0; i < container.size(); i++){
-        if (container.at(i) == element) return i;
     }
 
-    return -1;
-}
-
-
-int binary_search(vector<string> container, string element) {
-    // initiate bounds for the portion of the container being considered
-    int low_val = 0;
-    int high_val = container.size();
-
-    // runs until element is found or found to not be in container
-    while (true){
-        int mid_val = (high_val + low_val)/2;  // sets variable equal to the index between low_val and high_val
-
-        // changes bounds or returns index if found according to element's relation to mid_val
-        if (element.compare(container.at(mid_val)) > 0) low_val = mid_val + 1;
-        else if (element.compare(container.at(mid_val)) < 0) high_val = mid_val - 1;
-        else if (element == container.at(mid_val)) return mid_val;
-
-        // runs when element is not in container
-        if (mid_val == high_val) break;
-    }
-
-    return -1;
 }
